5ebf4cbe39720ffb55dba3d4916a260d314a1528
alex101 <hexuanhua101@gmail.com>
2019-08-27 13:01:21 +0800

    增加插入，查找接口实现

--- Tsinghua/列表/List.cpp
+++ Tsinghua/列表/List.cpp
@@ -23,6 +23,14 @@ template<typename T>class List
 		T& operator[](Rank r) const;
 		ListNodePosi(T) first() const{return header->succ;}
 		ListNodePosi(T) last() const{return trailer->pred;}
+		ListNodePosi(T) find(T const&e,int n,ListNodePosi(T)p) const;
+		ListNodePosi(T) find(T const &e){return find(e,_size,trailer);}
+		ListNodePosi(T) insertAsFirst(T const&e);
+		ListNodePosi(T) insertAsLast(T const&e);
+		ListNodePosi(T)	insertBefore(ListNodePosi(T) p,T const &e);
+		ListNodePosi(T) insertAfter(ListNodePosi(T) p,T const&e);
+		
+		
 } 
 template<typename T>void List<T>::init()
 {
@@ -34,11 +42,38 @@ template<typename T>void List<T>::init()
 	trailer->succ = NULL;
 	_size= 0;
 }
-template<typename T>
-T &List<T>::operator[](Rank r) const
+template<typename T> T &List<T>::operator[](Rank r) const
 {
 	ListNodePosi(T) p = first();
 	while(0<r--) p = p->succ;
 	return p->data;
+}
+template<typename T> ListNodePosit(T) List<T>::find(T const &e,int n,ListNodePosi(T) p) const
+{
+	while(0<n--)
+	{
+		if(e==(p=p->pred)->data) return p;
+	}
+	return NULL;
+}
+template<typename T> ListNodePosi(T) List<T>::insertAsFirst(T const&e)
+{
+	_size++;
+	return header->insertAsSucc(e);
+}
+template<typename T> ListNodePosi(T) List<T>::insertAsLast(T const&e)
+{
+	_size++;
+	return trailer->insertAsPred(e);
+}
+template<typename T> ListNodePosi(T) List<T>::insertBefore(ListNodePosi(T) p,T const &e)
+{
+	_size++;
+	return p->insertAsPred;
+}
+template<typename T> ListNodePosi(T) List<T>::insertAfter(ListNodePosi(T) p,T const&e)
+{
+	_size++;
+	return p->insertAsSucc(e);
 }
 
