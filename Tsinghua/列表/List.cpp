e3f4d7514473a3c375ad6294e1bb74d160d788a8
alex101 <hexuanhua101@gmail.com>
2019-08-29 00:34:07 +0800

    增加选择排序，插入排序算法

--- Tsinghua/列表/List.cpp
+++ Tsinghua/列表/List.cpp
@@ -14,6 +14,7 @@ template<typename T>class List
 		void mergeSort(ListNodePosi(T)&,int);
 		void selectionSort(ListNodePosi(T),int);
 		void insertionSort(ListNodePosi(T),int);
+		ListNodePosi(T) selectMax(ListNodePosi(T) p, int n);
 	public:
 		List(){init();}
 		List(List<T>const&L);
@@ -150,4 +151,37 @@ template<typename T> ListNodePosi(T) List<T>::search(T const&e,int n,ListNodePos
 	while(0<=n--)
 		if((p=p->pred)->data<=e) break;
 	return p;
+}
+template<typename T> void List<T>::insertionSort(ListNodePosi(T) p,int n)
+{
+	for(int r =0;r<n;r++)
+	{
+		insertAfter(search(p->data,r),p->data);
+		p= p->succ;
+		remove(p->pred);
+	}
+}
+template<typename T>void selectionSort(ListNodePosi(T),int n)
+{
+	ListNodePosi(T) head = p->pred;
+	ListNodePosi(T) tail = p;
+	for(int i=0;i<n;i++) tail = tail->succ;
+	while(1<n)
+	{
+		ListNodePosi(T) max = selectMax(head->succ,n);
+		insertBefore(tail,remove(max));
+		tail = tail->pred;
+	}
+}
+ListNodePosi(T) List<T>::selectMax(ListNodePosi(T) p, int n);
+{
+	ListNodePosi(T) max = p;
+	for(ListNodePosi(T) cur=p;1<n;n--)
+	{
+		if((cur=cur->succ)->data>=max->data)
+			{
+				max = cur;
+			}
+	}
+	return max;
 }
