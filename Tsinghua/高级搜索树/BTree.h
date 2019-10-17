c40b5c77603dd104fa0c25162d25c6896d6e8168
alex101 <hexuanhua101@gmail.com>
2019-10-17 16:00:05 +0800

    增加btree 插入实现

--- Tsinghua/高级搜索树/BTree.h
+++ Tsinghua/高级搜索树/BTree.h
@@ -7,7 +7,7 @@ template<typename T>class BTree
         BTNodePosi(T) _root;
         BTNodePosi(T) _hot;
         void solveOverflow(BTNodePosi(T));
-        Void solveUnderflow(BTNodePosi(T));
+        void solveUnderflow(BTNodePosi(T));
     public:
         BTree(int order=3):_order(order),_size(0)
         {
@@ -33,4 +33,62 @@ template<typename T>BTNodePosi(T) BTree<T>::search(const T& e)
         _hot = v;v=v->child[r+1];
     }
     return NULL;
+}
+template<typename T>bool BTree<T>::insert(const T&e)
+{
+    BTNodePosi(T) v = search(e);
+    if(v) return false;
+    int r = _hot->search(e);
+    _hot->key.insert(r+1,e);
+    _hot->child.insert(r+2,NULL);
+    _size++;
+    solveOverflow(_hot);
+    return true;
+}
+template<typename T>bool BTree<T>::remove(const T& e)
+{
+    BTNodePosi(T) v=search(e);
+    if(!v) return fasle;
+    int r = v->key.search(e);
+    if(v->child[0])
+    {
+        BTNodePosi(T) u = v->child[r+1];
+        while (u->child[0])
+        {
+            u = u->child[0];
+        }
+        v->key[r] = u->key[0];
+        v=u;
+        r=0;
+    }
+    v->key.remove(r);
+    v->child.remove(r+1);
+    _size--;
+    solveUnderflow(v);
+    return true;
+}
+template<typename T>void BTree<T>::solveOverflow(BTNodePosi(T) v)
+{
+    if(_order>=v->child.size()) return;
+    int s = _order/2;
+    BTNodePosi(T) u = new BTNode<T>();
+    for(int j=0;j<_order-s-1;j++)
+    {
+        u->child.insert(j,v->child.remove(s+1));
+        u->key.insert(j.v->key.remove(s+1));
+    }
+    u->child[_order-s-1] = v->child.remove(s+1);
+    if(u->child[0])
+    for (int j = 0; j < _order-s; j++)
+    {
+        u->child[j]->parent=u;
+    }
+    BTNodePosi(T) p =v->parent;
+    if(!p) {_root=p=new BTNode<T>();p->child[0]=v;v->parent=p;}
+    int r = 1+p->key.search(v->key[0]);
+    p->key.insert(r,v->key.remove(s));
+    p->child.insert(r+1,u);
+    u->parent  =p;
+    solveOverflow(p);
+    
 }
